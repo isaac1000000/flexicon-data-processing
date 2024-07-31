40c33e37af6e9965b73727b0a503e8b7414f33cc
Isaac Fisher <isaac.fisher@nyu.edu>
2024-07-31 12:07:42 -0400

    store calculated columns

--- database/create_indexes.sql
+++ database/create_indexes.sql
@@ -1,3 +1,4 @@
+ALTER TABLE ONLY rels ADD CONSTRAINT rels_pkey PRIMARY KEY (baseId, targetId);
 CREATE INDEX baseIdStrengthIndex ON rels (baseId, strength DESC);
 
 CREATE INDEX idFrequencyIndex ON words(id, frequency DESC);
--- database/store_calculated_columns.sql
+++ database/store_calculated_columns.sql
@@ -7,13 +7,18 @@ WITH maxCount AS (
 UPDATE words
 SET frequency = frequency(id, (SELECT max FROM maxCount));
 
-ALTER TABLE rels ADD COLUMN 
-strength NUMERIC(8,7);
+SELECT baseId, max(instances) INTO TEMPORARY TABLE groupMaxCount
+FROM rels
+GROUP BY baseId;
 
-WITH groupMaxCount AS (
-	SELECT baseId, max(instances)
-	FROM rels
-	GROUP BY baseId
-)
-UPDATE rels
-SET strength = strength(instances, (SELECT max FROM groupMaxCount WHERE baseId = rels.baseId));
+CREATE INDEX ididx ON groupMaxCount (baseId);
+
+CREATE TABLE rels2 AS
+SELECT *, strength(instances, (SELECT max FROM groupMaxCount WHERE baseId=rels.baseId)) FROM rels;
+
+DROP TABLE rels;
+
+ALTER TABLE rels2 RENAME TO rels;
+
+ALTER TABLE ONLY rels ADD CONSTRAINT rels_pkey PRIMARY KEY (baseId, targetId);
+CREATE INDEX baseIdStrengthIndex ON rels (baseId, strength DESC);
